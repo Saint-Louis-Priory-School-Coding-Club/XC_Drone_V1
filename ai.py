74afbc6c6da5ff51e71f6119069f895b8cc69302
hexcodefff <minecrafthex213@gmail.com>
2019-11-23 17:26:05 -0600

    what the fuck did i just write

--- ai.py
+++ ai.py
@@ -1,10 +1,9 @@
 # pip install opencv-python
 # pip install numpy
-import colorsys
 import cv2
 import numpy as np
 
-write_to_file = True  # if this is true, it writes any frames detected with a marker to an avi for training
+write_to_file = False  # if this is true, it writes any frames detected with a marker to an avi for training
 # filename
 name = "out"  # .avi
 # definitely not code copied from github that shows the webcam in cv2
@@ -47,10 +46,14 @@ def create_blob_detector():
 
 
 hue = 0
-hue_tolerance = 5  # hue plus or minus this value (hue is 0-360)
-lower = np.array([hue - hue_tolerance, 128, 128])
-upper = np.array([hue + hue_tolerance, 256, 256])
-print(lower, upper)
+hue *= 2
+hue_tolerance = 5  # hue plus or minus this value (hue is 0-180)
+# lower = np.array([hue - hue_tolerance, 128, 128])
+# upper = np.array([hue + hue_tolerance, 256, 256])
+lower1 = np.array([0, 128, 128])
+upper1 = np.array([5, 256, 256])
+lower2 = np.array([175, 128, 128])
+upper2 = np.array([180, 256, 256])
 
 name += '.avi'
 # fourcc = cv2.VideoWriter_fourcc(*'MP4V')
@@ -66,12 +69,16 @@ while True:
         img = cv2.flip(img, 1)
     # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(img, lower, upper)
+    # mask = cv2.inRange(img, lower, upper)
+    mask1 = cv2.inRange(img, lower1, upper1)
+    mask2 = cv2.inRange(img, lower2, upper2)
+    mask = cv2.bitwise_or(mask1, mask2)
     img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
-    mask = cv2.dilate(mask, kernel, iterations=3)
+    invmask = (255 - mask)
+    # mask = cv2.dilate(mask, kernel, iterations=3)
     res = cv2.bitwise_and(img, img, mask=mask)
-    # invmask = (255 - mask)
-    # nored = cv2.bitwise_and(img, img, mask=invmask)
+
+    nored = cv2.bitwise_and(img, img, mask=invmask)
 
     keypoints = detector.detect(mask)
 
@@ -86,13 +93,59 @@ while True:
         y1 = max(0, round(keyp.pt[1] - keyp.size / 2))
         y2 = max(0, round(keyp.pt[1] + keyp.size / 2))
         crop_img = img.copy()[y1:y2, x1:x2]
-        # nored = nored.copy()[y1:y2, x1:x2]
+        nored = nored.copy()[y1:y2, x1:x2]
         cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 3)
+        height, width, channels = crop_img.shape
+        crop_imghsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+        hues = create_blank(height, width)
+        printline = ""
+        state = "nocode"
+        awaitingstate = "nocode"
+        timeinstate = 0
+        timewaiting = 0
+        for pixel in range(height):
+            pixelraw = crop_imghsv[pixel][width // 2]
+            hue = pixelraw[0] * 2
+            if pixelraw[1] > 50 and pixelraw[2] > 50:
+                linehue = (int(hue / 2), 255, 255)
+                if awaitingstate == "white":
+                    awaitingstate = "nocode"
+                    timewaiting = 0
+                if 30 < hue < 70:
 
+                    if state == "nocode" and awaitingstate == "nocode":
+                        awaitingstate = "yellow"
+                        timewaiting = 0
+                    if awaitingstate == "yellow" and timewaiting > 2:
+                        state = "yellow"
+                        timeinstate = 0
+                        timewaiting = 0
+                        printline += "code: "
+                # if 230 < hue < 260:
+
+            # print(hue)
+            else:
+                linehue = (0, 0, 255)
+
+                if awaitingstate is not "white":
+                    awaitingstate = "white"
+                    timewaiting = 0
+                if timewaiting > 5:
+                    state = "white"
+            # linehue = (int(crop_imghsv[pixel][width // 2][0] * 2), 100, 100)
+            # linehue = cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+            # linehue = colorsys.rgb_to_hsv(linehue, 50, 50)
+            # linehue = (linehue, 50, 50)
+            timeinstate += 1
+            timewaiting += 1
+            cv2.rectangle(hues, (0, int(pixel)), (int(width), int(pixel)), linehue, 1)
+        print(printline)
+        hues = cv2.cvtColor(hues, cv2.COLOR_HSV2BGR)
         # crop_img = res[1:100, 1:100]
     else:
         crop_img = create_blank(100, 100)
-        # nored = create_blank(100, 100)
+        nored = create_blank(100, 100)
+        hues = create_blank(100, 100)
     img = cv2.drawKeypoints(img, keypoints, np.array([]), (255, 255, 0),
                             cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     BLUE = [255, 255, 255]
@@ -100,7 +153,8 @@ while True:
     if write_to_file and len(keypoints) > 0:
         # frame = cv2.flip(crop_img, 0)
         out.write(crop_img)
-    # nored = cv2.resize(nored, (100, 100), interpolation=cv2.INTER_AREA)
+    nored = cv2.resize(nored, (100, 100), interpolation=cv2.INTER_AREA)
+    # hues = cv2.resize(hues, (100, 100), interpolation=cv2.INTER_AREA)
     '''colorcode = nored.mean(axis=0).mean(axis=0)
     hsvcoder = colorsys.rgb_to_hsv(colorcode[2] / 255, colorcode[1] / 255, colorcode[0] / 255)
     hsvcode = [round(hsvcoder[0] * 360), round(hsvcoder[1] * 100), round(hsvcoder[2] * 100)]
@@ -117,7 +171,9 @@ while True:
     cv2.imshow('frame', img)
     # cv2.imshow('mask', mask)
     cv2.imshow('res', res)
-    # cv2.imshow("nored", nored)
+    cv2.imshow("nored", nored)
+    # crop_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+    cv2.imshow("ohno", hues)
     # cv2.imshow('blobs', im_with_keypoints)
     # cv2.imshow('blob', im_with_keypoints)
     if cv2.waitKey(1) == 27:
